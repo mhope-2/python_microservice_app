7b194beefe51add178469176ced641eca7bf79a9
mhope <mhope@npontu.com>
2021-05-25 20:11:29 +0000

    feat(product): added product app - viewset and urls

--- README.md
+++ README.md
@@ -12,4 +12,4 @@
 ## Architecture:
 ![Architecture Image](/media/python_uS_arch.png?raw=true) 
 
-#### reference: https://www.youtube.com/watch?v=0iB5IPoTDts (FreeCodeCamp)
+#### reference: https://www.youtube.com/watch?v=0iB5IPoTDts ( FreeCodeCamp )
--- django_admin/products/views.py
+++ django_admin/products/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product
+from .serializer import ProductSerializer
+
 # Create your views here.
 
 class ProductViewSet(viewsets.ViewSet):
     def __init__(self): 
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
     def list(self, request): #/api/products/ -- GET
         pass
